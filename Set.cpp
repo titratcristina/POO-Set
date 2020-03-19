fb9ed934bd9c7d6d48951b0fbf77970b04001972
Titrat Cristina <titratcristina@gmail.com>
2020-03-19 18:02:53 +0200

    Init project

---
+++ Set.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cristina Titrat on 19/03/2020.
+//
+
+#include "Set.h"
---
+++ Set.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 19/03/2020.
+//
+
+#ifndef POO_SET_SET_H
+#define POO_SET_SET_H
+
+
+class Set {
+
+};
+
+
+#endif //POO_SET_SET_H
