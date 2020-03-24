dd35fc0a29633df817bf7b7b267f972e13d47fbf
Titrat Cristina <titratcristina@gmail.com>
2020-03-24 10:49:10 +0200

    Add Node class

---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cristina Titrat on 24/03/2020.
+//
+
+#include "Node.h"
---
+++ Node.h
@@ -0,0 +1,14 @@
+//
+// Created by Cristina Titrat on 24/03/2020.
+//
+
+#ifndef POO_SET_NODE_H
+#define POO_SET_NODE_H
+
+
+class Node {
+
+};
+
+
+#endif //POO_SET_NODE_H
