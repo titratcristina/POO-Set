ed182a559abcee9b17477ea2134b7ba410f273f2
Titrat Cristina <titratcristina@gmail.com>
2020-03-24 18:47:49 +0200

    Beautify comments

--- Set.h
+++ Set.h
@@ -5,8 +5,8 @@
 #ifndef POO_SET_SET_H
 #define POO_SET_SET_H
 
-#include "Set.h"
 #include "Node.h"
+#include <exception>
 #include <iostream>
 #include <ostream>
 
@@ -16,31 +16,29 @@ class Set {
 	Node *array;
 	unsigned size;
 
-	bool has(int);		// funcție pentru a verifica dacă un element se află în set
-	int get(int);       // ia elementul de pe poziția dată
+	bool has(int);        		// funcție pentru a verifica dacă un element se află în set
+	int get(int);       		// ia elementul de pe poziția dată
 
 public:
 
-	//  TODO
-
 	//  Implementatea constructorilor cu parametri, fără parametri, de copiere.
-	Set();					// constructor fără parametri
-	Set(int);				// constructor cu un parametru
-	Set(int, int);			// constructor cu doi parametri (inserează un element pe o poziție dată)
-	Set(Set &);				// constructor de copiere
+	Set();                    	// constructor fără parametri
+	Set(int);                	// constructor cu un parametru
+	Set(int, int);            	// constructor cu doi parametri (inserează un element pe o poziție dată)
+	Set(Set &);                	// constructor de copiere
 
 	//  Implementarea destructorului.
 	~Set();
 
 	//  Inserarea, ștergerea și căutarea unui element.
-	void insertAt(int, int);		// inserează un element pe pozitia specificată
-	void insert(int);               // inserează un element pe ultima poziție
-	void remove(int);               // sterge elementul de pe poziția dată ca parametru
-	int find(int);                  // returnează poziția elementului
+	void insertAt(int, int);	// inserează un element pe pozitia specificată
+	void insert(int);           // inserează un element pe ultima poziție
+	void remove(int);           // sterge elementul de pe poziția dată ca parametru
+	int find(int);              // returnează poziția elementului
 
 	//  Obținerea elementelor pare, respectiv impare din mulțime (sub formă de Set).
-	Set getEven();		// returnează un set nou cu elementele pare din mulțimea inițială
-	Set getOdd();		// returnează un set nou cu elementele impare din mulțimea inițială
+	Set getEven();        		// returnează un set nou cu elementele pare din mulțimea inițială
+	Set getOdd();        		// returnează un set nou cu elementele impare din mulțimea inițială
 
 	//  Obținerea sumei elementelor din mulțime.
 	int sum();
@@ -49,7 +47,7 @@ public:
 	unsigned length();
 
 	//  Supraı̂ncărcarea operatorului =.
-	void operator = (Set&);
+	void operator=(Set &);
 
 	//  Supraîncărcarea operatorului [] pentru accesarea elementului de pe poziția i.
 	int operator[](unsigned);
